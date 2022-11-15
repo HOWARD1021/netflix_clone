c22add98a40f59ca902ef97b4888f83044c076ba
HOWARD1021 <44057435+HOWARD1021@users.noreply.github.com>
2022-11-15 12:23:39 +0800

    digitaloceantest

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,13 +3,17 @@ import Head from 'next/head'
 import Image from 'next/image'
 import Header from '../components/Header'
 import Banner from '../components/Banner'
+import Plans from '../components/Plans'
 import Row from '../components/Row'
 import Modal from '../components/Modal'
 import requests from '../utils/request'
 import { Movie } from '../typings'
 import useAuth from '../hooks/useAuth'
+import useSubscription from '../hooks/useSubscription'
 import { useRecoilValue } from 'recoil'
 import { modalState } from '../atoms/modalAtom'
+import { getProducts, Product } from '@stripe/firestore-stripe-payments'
+import payments from '../lib/stripe'
 interface Props {
   netflixOriginals: Movie[]
   trendingNow: Movie[]
@@ -19,6 +23,7 @@ interface Props {
   horrorMovies: Movie[]
   romanceMovies: Movie[]
   documentaries: Movie[]
+  products:Product[]
 }
 const Home = (
   {
@@ -30,11 +35,16 @@ const Home = (
     romanceMovies,
     topRated,
     trendingNow,
+    products
   }: Props
   ) => {
-    const {logout, loading} = useAuth()
-    const showModal = useRecoilValue(modalState)
-    if(loading) return 'Loading'
+    console.log(products,'products');    
+    const {logout, loading ,user} = useAuth()
+    const subscription = useSubscription(user)
+    const showModal = useRecoilValue(modalState)    
+    if(loading || subscription === null) return null 
+    if (!subscription) return <Plans products={products} />
+
     return (
     <div className={`relative h-screen bg-gradient-to-b from-gray-900/10
      to-[#010511] lg:h-[140vh] 
@@ -43,8 +53,8 @@ const Home = (
         <title>Netflix</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="relative  pb-24 lg:space-y-24  ">      
       <Header/>
+      <main className="relative  pb-24 lg:space-y-24  ">      
        
       <Banner netflixOriginals={netflixOriginals} />
       <section className="md:space-y-24  ">
@@ -68,7 +78,11 @@ const Home = (
 export default Home
 
 
-export const getServerSideProps  = async() =>{  
+export const getStaticProps  = async() =>{  
+    const products =  await getProducts (payments, {
+      includePrices:true,
+      activeOnly:true
+    }).then((res) => res).catch( (error) => console.log('error', error.message)) 
   const [
     netflixOriginals,
     trendingNow,
@@ -78,6 +92,7 @@ export const getServerSideProps  = async() =>{
     horrorMovies,
     romanceMovies,
     documentaries,
+    
   ] = await Promise.all([
     fetch(requests.fetchNetflixOriginals).then((res) => res.json()),
     fetch(requests.fetchTrending).then((res) => res.json()),
@@ -98,7 +113,8 @@ export const getServerSideProps  = async() =>{
       comedyMovies: comedyMovies.results,
       horrorMovies: horrorMovies.results,
       romanceMovies: romanceMovies.results,
-      documentaries: documentaries.results,      
+      documentaries: documentaries.results,     
+     products
     },
   }
 }
